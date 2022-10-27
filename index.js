6ba5296f996032362e668ad48f45caecf60a5005
BEBACK100 <beback_bain@yahoo.com>
2022-10-27 21:49:18 +0600

    path name commit

--- index.js
+++ index.js
@@ -13,6 +13,21 @@ app.get('/', (req, res) => {
 app.get('/displayData', (req, res) => {
     res.send(myfakeData)
 })
+app.get('/physics', (req, res) => {
+    res.send(myfakeData)
+})
+app.get('/chemistry', (req, res) => {
+    res.send(myfakeData)
+})
+app.get('/math', (req, res) => {
+    res.send(myfakeData)
+})
+app.get('/ict', (req, res) => {
+    res.send(myfakeData)
+})
+app.get('/programminglanguage', (req, res) => {
+    res.send(myfakeData)
+})
 app.listen(port, () => {
     console.log('My online tuition platform in running,', port)
 })
